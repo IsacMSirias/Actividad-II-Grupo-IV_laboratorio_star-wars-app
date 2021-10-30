e60fefb3f646559d23c37b645f9c2d30241672db
AlbertVega <85046754+AlbertVega@users.noreply.github.com>
2021-10-29 22:39:38 -0600

    Version final

--- src/main/java/GUI/Tabla.java
+++ src/main/java/GUI/Tabla.java
@@ -1,32 +1,31 @@
 package GUI;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileReader;
 import java.util.Date;
 
 public class Tabla extends  JFrame{
 
     JFrame tabla = new JFrame();
     JTable tablaStarWars = new JTable();
+    public static Object datos;
+    private static String[][] arraydePersonajes = new String[5][3];
 
 
     public Tabla() {
         super("StarWarsTable");
+        ReadJson();
 
-        Object[][] datosfila = {
+        String[] IDColumnas = {"Nombre", "Altura", "Fecha de Nacimiento"};
 
-                {"Luke SkywalkerLuke Skywalker", "1.75 m", "25 de septiembre de 1951"},
-                {"Leia Organa", "1.55 m", "21 de October de 1956"},
-                {"Darth Vader", " 2.18 m", "19 de mayo de 1944"},
-                {"Han Solo", "1.85 m", "13 de julio de 1942"}
-
-        };
-
-        String[] IDColumnas = {"Nombre", "Altura", "Año de Nacimiento"};
-
-        final JTable table = new JTable(datosfila, IDColumnas);
+        final JTable table = new JTable(arraydePersonajes, IDColumnas);
         table.setPreferredScrollableViewportSize(new Dimension(700, 80));
         JScrollPane scrollpane = new JScrollPane(table);
 
@@ -39,5 +38,31 @@ public class Tabla extends  JFrame{
             }
         });
 
+    }
+
+    public static void ReadJson(){
+        JSONParser parser = new JSONParser();
+        try {
+            Object objeto = parser.parse(new FileReader("src/main/java/datos.json"));
+
+            JSONArray json = (JSONArray) objeto;
+
+            for (int i = 0; i < json.size(); i++) {
+                String[] arrayanidado = new String[3];
+                JSONObject object =(JSONObject) json.get(i);
+                String edad = object.get("Fecha de nacimiento").toString();
+                String estatura = object.get("Estatura").toString();
+                String nombre = object.get("Nombre").toString();
+
+                arrayanidado[2] = edad;
+                arrayanidado[1] = estatura;
+                arrayanidado[0] = nombre;
+
+                arraydePersonajes[i] = arrayanidado;
+            }
+
+        } catch (Exception e) {
+
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
 import GUI.Tabla;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
 
         Tabla gui = new Tabla();
         gui.pack();
         gui.setVisible(true);
-
         System.out.println("Hello world");
 
+        logger.info("{} ha sido cargada", Main.class.getSimpleName());
+        logger.warn("{} ha sido cargada", Main.class.getSimpleName());
+        logger.debug("{} ha sido cargada", Main.class.getSimpleName());
+        logger.error("{} ha sido cargada", Main.class.getSimpleName());
+
     }
+
 }
