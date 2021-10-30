5252a32c4ae09e20c145faf905c6a33eb0976fd5
Zorrox0117 <83249731+Zorrox0117@users.noreply.github.com>
2021-10-29 20:08:37 -0700

    Revert "version que hay que arreglar Json"
    This reverts commit 3ebd2ab7c5e69d8788aef4d1decd598d869b0d86.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,6 @@
-
-import ch.qos.logback.classic.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.slf4j.LoggerFactory;
 import GUI.Tabla;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
@@ -19,13 +10,6 @@ public class Main {
 
         System.out.println("Hello world");
 
-        logger.info("{} ha sido cargada", Main.class.getSimpleName());
-        logger.debug("{} ha sido cargada", Main.class.getSimpleName());
-        logger.error("{} ha sido cargada", Main.class.getSimpleName());
-        logger.warn("{} ha sido cargada", Main.class.getSimpleName());
-
     }
 
 }
-
-
