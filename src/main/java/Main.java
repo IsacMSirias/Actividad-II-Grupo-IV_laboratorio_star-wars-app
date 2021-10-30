fad523f9f63dd6471f5ebbfeab074fb18b8d1552
Zorrox0117 <83249731+Zorrox0117@users.noreply.github.com>
2021-10-29 20:08:46 -0700

    Revert "DataTable"
    This reverts commit 14a88be5ad640fd252a5cdf61c85c8cd653a20c4.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
-import GUI.Tabla;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        Tabla gui = new Tabla();
-        gui.pack();
-        gui.setVisible(true);
 
         System.out.println("Hello world");
 
