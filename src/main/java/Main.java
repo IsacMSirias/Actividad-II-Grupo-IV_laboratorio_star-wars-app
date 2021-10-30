7659a8c73c59392a521ad1cff2dbba1f066fbf34
Zorrox0117 <83249731+Zorrox0117@users.noreply.github.com>
2021-10-29 20:10:55 -0700

    Revert hasta donde funcaba :v

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import GUI.Tabla;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Tabla gui = new Tabla();
+        gui.pack();
+        gui.setVisible(true);
+
         System.out.println("Hello world");
 
     }
-
 }
